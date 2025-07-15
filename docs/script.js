88665d7b594d359992cea4240402949e41d24b03
Dineshreddy572 <kanipakamdinesh2004@gmail.com>
2025-07-15 14:41:06 +0530

    Delete docs/script.js

--- docs/script.js
+++
@@ -1,9 +0,0 @@
-function buyNow(productName) {
-  alert("Thank you for showing interest in " + productName + "! We’ll add payment soon.");
-}
-function addToCart(name, price) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push({ name, price });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(name + " added to cart!");
-}
